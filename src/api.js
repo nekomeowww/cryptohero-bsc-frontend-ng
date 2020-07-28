23170112cd9dee27a79d2dca2810a00d1170ea0e
Frank Wei <frank@frankwei.xyz>
2020-07-28 18:17:51 +0800

    refactor: disabled leancloud since no data

--- src/api.js
+++ src/api.js
@@ -3,7 +3,6 @@ import Cookie from "js-cookie";
 // import { BigNumber } from 'bignumber.js';
 import web3 from "@/web3";
 import * as config from "@/config";
-import request from "superagent";
 import timeout from "timeout-then";
 import cryptoWaterMarginABI from "./abi/cryptoWaterMargin.json";
 import convertContractABI from "./abi/convertContract.json";
@@ -23,24 +22,9 @@ const convertContract = new web3.eth.Contract(
 let store = [];
 let isInit = false;
 
-export const init = async () => {
-  await request
-    .get("https://api.leancloud.cn/1.1/classes/ad")
-    .set({
-      "X-LC-Id": "R6A46DH2meySCVNM1uWOoW2M-gzGzoHsz",
-      "X-LC-Key": "8R6rGgpHa0Y9pq8uO53RAPCB"
-    })
-    .type("json")
-    .accept("json")
-    .then(response => {
-      if (response.body && response.body.results) {
-        store = response.body.results;
-      }
-      isInit = true;
-    });
+export const getStoreData = async () => {
+  // @todo: impl needed
 };
-
-init().then();
 
 export const getMe = async () => {
   if (!window.ethereum) {
@@ -51,17 +35,6 @@ export const getMe = async () => {
 };
 
 export const getAnnouncements = async () => {
-  const response = await request
-    .get("https://api.leancloud.cn/1.1/classes/announcement")
-    .set({
-      "X-LC-Id": "R6A46DH2meySCVNM1uWOoW2M-gzGzoHsz",
-      "X-LC-Key": "8R6rGgpHa0Y9pq8uO53RAPCB"
-    })
-    .type("json")
-    .accept("json");
-  if (response.body && response.body.results) {
-    return response.body.results;
-  }
   return [];
 };
 
@@ -70,7 +43,7 @@ export const getGg = async (id, time = 0) => {
     return timeout((time + 1) * 500).then(() => getGg(id, time + 1));
   }
 
-  const item = store.find(x => x.id === `${id}`);
+  const item = store.find((x) => x.id === `${id}`);
 
   if (item && item.str) {
     return item.str;
@@ -80,51 +53,51 @@ export const getGg = async (id, time = 0) => {
 };
 
 export const setGg = async (id, str) => {
-  const response = await request
-    .get("https://api.leancloud.cn/1.1/classes/ad")
-    .set({
-      "X-LC-Id": "R6A46DH2meySCVNM1uWOoW2M-gzGzoHsz",
-      "X-LC-Key": "8R6rGgpHa0Y9pq8uO53RAPCB"
-    })
-    .type("json")
-    .accept("json");
-  if (response.body && response.body.results) {
-    store = response.body.results;
-  }
-  const item = store.find(x => x.id === `${id}`);
-  if (item) {
-    // update
-    await request
-      .put(`https://api.leancloud.cn/1.1/classes/ad/${item.objectId}`)
-      .set({
-        "X-LC-Id": "R6A46DH2meySCVNM1uWOoW2M-gzGzoHsz",
-        "X-LC-Key": "8R6rGgpHa0Y9pq8uO53RAPCB"
-      })
-      .type("json")
-      .accept("json")
-      .send({
-        str
-      });
-    // update store
-    item.str = str;
-  } else {
-    // create
-    await request
-      .post("https://api.leancloud.cn/1.1/classes/ad")
-      .set({
-        "X-LC-Id": "R6A46DH2meySCVNM1uWOoW2M-gzGzoHsz",
-        "X-LC-Key": "8R6rGgpHa0Y9pq8uO53RAPCB"
-      })
-      .type("json")
-      .accept("json")
-      .send({
-        id: `${id}`,
-        str
-      });
-    // update store
-    await init();
-  }
-  return str;
+  // @todo: need impl
+  // const response = await request
+  //   .get("https://api.leancloud.cn/1.1/classes/ad")
+  //   .set({
+  //     "X-LC-Id": "R6A46DH2meySCVNM1uWOoW2M-gzGzoHsz",
+  //     "X-LC-Key": "8R6rGgpHa0Y9pq8uO53RAPCB",
+  //   })
+  //   .type("json")
+  //   .accept("json");
+  // if (response.body && response.body.results) {
+  //   store = response.body.results;
+  // }
+  // const item = store.find((x) => x.id === `${id}`);
+  // if (item) {
+  //   // update
+  //   await request
+  //     .put(`https://api.leancloud.cn/1.1/classes/ad/${item.objectId}`)
+  //     .set({
+  //       "X-LC-Id": "R6A46DH2meySCVNM1uWOoW2M-gzGzoHsz",
+  //       "X-LC-Key": "8R6rGgpHa0Y9pq8uO53RAPCB",
+  //     })
+  //     .type("json")
+  //     .accept("json")
+  //     .send({
+  //       str,
+  //     });
+  //   // update store
+  //   item.str = str;
+  // } else {
+  //   // create
+  //   await request
+  //     .post("https://api.leancloud.cn/1.1/classes/ad")
+  //     .set({
+  //       "X-LC-Id": "R6A46DH2meySCVNM1uWOoW2M-gzGzoHsz",
+  //       "X-LC-Key": "8R6rGgpHa0Y9pq8uO53RAPCB",
+  //     })
+  //     .type("json")
+  //     .accept("json")
+  //     .send({
+  //       id: `${id}`,
+  //       str,
+  //     });
+  //   // update store
+  // }
+  return "";
 };
 
 // 获取此卡片的推荐nextPrice，需要和卡片blockchain上的nextPrice进行比较，选择较大的创建交易
@@ -137,7 +110,7 @@ export const getNextPrice = async (id, time = 0) => {
     return timeout((time + 1) * 500).then(() => getNextPrice(id, time + 1));
   }
 
-  const item = store.find(x => x.id === `${id}`);
+  const item = store.find((x) => x.id === `${id}`);
 
   if (item && item.nextPrice) {
     // Convert nextPrice from 'ether' to 'wei'
@@ -148,68 +121,25 @@ export const getNextPrice = async (id, time = 0) => {
 };
 
 // price为用户成功发起交易的交易价格，调用setNextPrice后，nextPrice会变为此价格的1.1倍
-export const setNextPrice = async (id, priceInWei) => {
-  // Convert price(Wei) to a number instance (ether)
-  const price = Number(web3.utils.fromWei(priceInWei, "ether").toString());
-  const response = await request
-    .get("https://api.leancloud.cn/1.1/classes/ad")
-    .set({
-      "X-LC-Id": "R6A46DH2meySCVNM1uWOoW2M-gzGzoHsz",
-      "X-LC-Key": "8R6rGgpHa0Y9pq8uO53RAPCB"
-    })
-    .type("json")
-    .accept("json");
-  if (response.body && response.body.results) {
-    store = response.body.results;
-  }
-  const item = store.find(x => x.id === `${id}`);
-  if (item) {
-    if (price <= item.nextPrice) {
-      return item.nextPrice;
-    }
-    // update
-    await request
-      .put(`https://api.leancloud.cn/1.1/classes/ad/${item.objectId}`)
-      .set({
-        "X-LC-Id": "R6A46DH2meySCVNM1uWOoW2M-gzGzoHsz",
-        "X-LC-Key": "8R6rGgpHa0Y9pq8uO53RAPCB"
-      })
-      .type("json")
-      .accept("json")
-      .send({
-        nextPrice: price * 1.1
-      });
-    // update store
-    item.nextPrice = price * 1.1;
-  } else {
-    await request
-      .post("https://api.leancloud.cn/1.1/classes/ad")
-      .set({
-        "X-LC-Id": "R6A46DH2meySCVNM1uWOoW2M-gzGzoHsz",
-        "X-LC-Key": "8R6rGgpHa0Y9pq8uO53RAPCB"
-      })
-      .type("json")
-      .accept("json")
-      .send({
-        id: `${id}`,
-        nextPrice: price * 1.1
-      });
-    // update store
-    await init();
-  }
+export const setNextPrice = async (id, price) => {
+  // @todo: need rewrite
   return price * 1.1;
 };
 
-export const getItem = async id => {
+export const getItem = async (id) => {
   const exist = await cryptoWaterMarginContract.methods.tokenExists(id).call();
   if (!exist) return null;
   const card = config.cards[id] || {};
   const item = {
     id,
     name: card.name,
-    nickname: card.nickname
+    nickname: card.nickname,
   };
-  [item.owner, item.price, item.nextPrice] = await cryptoWaterMarginContract.methods.allOf(id).call();
+  [
+    item.owner,
+    item.price,
+    item.nextPrice,
+  ] = await cryptoWaterMarginContract.methods.allOf(id).call();
 
   // [[item.owner, item.price, item.nextPrice], item.estPrice] = await Promise.all([
   //   Promise.promisify(cryptoWaterMarginContract.methods.allOf)(id),
@@ -218,48 +148,50 @@ export const getItem = async id => {
   return item;
 };
 
-export const buyItem = (id, price) => cryptoWaterMarginContract.methods.buy(id).send({
+export const buyItem = (id, price) =>
+  cryptoWaterMarginContract.methods.buy(id).send({
     value: price, // web3.utils.toWei(Number(price), 'ether'),
     gas: 220000,
-    gasPrice: 1000000000 * 100
-});
+    gasPrice: 1000000000 * 100,
+  });
 
 // Lucky Part
-export const exchangeLuckyToken = tokenId =>
+export const exchangeLuckyToken = (tokenId) =>
   convertContract.getNewToken(tokenId).send({
     value: 0, // web3.toWei(Number(price), 'ether'),
     gas: 80000,
-    gasPrice: 1000000000 * 18
+    gasPrice: 1000000000 * 18,
   });
 
-export const isConvert = cardId =>
+export const isConvert = (cardId) =>
   new Promise((resolve, reject) => {
     convertContract.isConvert(cardId, (err, result) =>
       err ? reject(err) : resolve(result)
     );
   });
 
-export const getTotal = () =>cryptoWaterMarginContract.methods.totalSupply().call();
+export const getTotal = () =>
+  cryptoWaterMarginContract.methods.totalSupply().call();
 
 export const getItemIds = async (offset, limit) => {
-  let ids = await cryptoWaterMarginContract.methods.itemsForSaleLimit(offset, limit).call();
-  ids = ids.map(id => id.toNumber());
+  let ids = await cryptoWaterMarginContract.methods
+    .itemsForSaleLimit(offset, limit)
+    .call();
+  ids = ids.map((id) => id.toNumber());
   ids.sort((a, b) => a - b);
   return Array.from(new Set(ids));
 };
 
-export const isItemMaster = async id => {
+export const isItemMaster = async (id) => {
   const me = await getMe();
   const item = await getItem(id);
 
   return me && me.address && item && item.owner && me.address === item.owner;
 };
 
-export const getItemsOf = async address => {
-  let ids = await cryptoWaterMarginContract.methods.tokensOf(
-    address
-  ).call();
-  ids = ids.map(id => id.toNumber());
+export const getItemsOf = async (address) => {
+  let ids = await cryptoWaterMarginContract.methods.tokensOf(address).call();
+  ids = ids.map((id) => id.toNumber());
   ids.sort((a, b) => a - b);
   return Array.from(new Set(ids));
 };
@@ -277,6 +209,6 @@ export const getLocale = async () =>
     navigator.userLanguage
   ).toLowerCase();
 
-export const setLocale = async locale => {
+export const setLocale = async (locale) => {
   Cookie.set("locale", locale, { expires: 365 });
 };
