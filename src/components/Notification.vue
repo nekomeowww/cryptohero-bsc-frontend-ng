7b571520055b52b6128420d023eb304704f1c482
Frank Wei <frank@frankwei.xyz>
2020-08-06 15:38:33 +0800

    refactor: rewrite as a component and reduce codes

---
+++ src/components/Notification.vue
@@ -0,0 +1,10 @@
+<template>
+  <article class="message is-info">
+    <div class="message-body">
+      <h4 class="title is-5">
+        <slot name="title"></slot>
+      </h4>
+      <slot></slot>
+    </div>
+  </article>
+</template>
