798b7f03c826a77ee6c8b965875ea17bd49710a3
Frank Wei <frank@frankwei.xyz>
2020-08-06 15:00:33 +0800

    feat: step by step notifications

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -72,16 +72,56 @@
                 {{ $t("PREMIUM_BUY_BTN", { rate: "50%" }) }}
               </button> -->
             </div>
-            <article class="message is-danger" v-if="payTokenInfo">
-              <div class="message-body">
-                这个卡牌需要 {{ payTokenInfo.name }}币 ({{
-                  payTokenInfo.symbol
-                }}) 才能购买
-                <br />
-                购买步骤： 1. 先授权我们合约花费指定金额 2.
-                调用我们合约去扣除你的 ERC20 代币。
-              </div>
-            </article>
+            <div class="notifications" v-if="payTokenInfo">
+              <article class="message is-info" v-if="buyStep === 0">
+                <div class="message-body">
+                  
+                    这个卡牌需要 {{ payTokenInfo.name }}币 ({{
+                    payTokenInfo.symbol
+                  }}) 才能购买
+                  <br />
+                  购买步骤：
+                  <br />
+                  1. 先授权我们合约花费指定金额 {{
+                    payTokenInfo.symbol
+                  }} <br />
+                  2. 调用我们合约去扣除你的 ERC20 代币。
+                </div>
+              </article>
+              <article class="message is-primary" v-else-if="buyStep === 1">
+                <div class="message-body">
+                  需要消费授权
+                  <br />
+                  正在使用 {{ payTokenInfo.name }}币 ({{ payTokenInfo.symbol }})
+                  购买这张卡牌，
+                  <br />请授权（Approve）我们的卡牌合约从你的账户中扣除
+                  {{ toDisplayedPrice(item.price) }}。
+                  <br />
+                  确认授权后，稍后会推送交易结果，授权成功后会自动进入购买程序，请不要离开。
+                </div>
+              </article>
+              <article class="message is-primary" v-else-if="buyStep === 2">
+                <div class="message-body">
+                  请确认你的交易
+                  <br />
+                  点击 Confirm 确认交易，稍后会推送交易结果，请不要离开。
+                </div>
+              </article>
+              <article class="message is-success" v-else-if="buyStep === 3">
+                <div class="message-body">
+                  购买成功
+                  <br />
+                  请稍后手动刷新页面，即可确认卡牌购买情况。
+                </div>
+              </article>
+              <article class="message is-danger" v-else>
+                <div class="message-body">
+                  购买失败
+                  <br />
+                  发生了一些问题，请联系开发者。
+                </div>
+              </article>
+            </div>
           </template>
 
           <template v-if="isOwner">
@@ -90,7 +130,7 @@
                 {{ $t("Edit Slogan") }}
               </button>
             </div> -->
-            <p>You owned this card.</p>
+            <p>你拥有这个卡牌。</p>
           </template>
         </div>
       </div>
@@ -100,14 +140,16 @@
 </template>
 
 <script>
-import { buyItem, setNextPrice } from "@/api";
+import { buyItem, approveSpending, setNextPrice } from "@/api";
 import { toReadablePrice } from "@/util";
 import { mapState } from "vuex";
 
 export default {
   name: "item-view",
 
-  data: () => ({}),
+  data: () => ({
+    buyStep: 0
+  }),
 
   computed: {
     ...mapState(["payTokenInfo"]),
@@ -150,20 +192,24 @@ export default {
   watch: {},
 
   methods: {
-    onBuy(rate) {
+    async onBuy(rate) {
       if (this.$store.state.signInError) {
         return this.$router.push({ name: "Login" });
       }
       const buyPrice = (this.item.price * rate).toFixed(0);
-      buyItem(this.itemId, buyPrice, this.me)
-        .then(() => {
-          alert(this.$t("BUY_SUCCESS_MSG"));
-          setNextPrice(this.itemId, buyPrice);
-        })
-        .catch(e => {
-          alert(this.$t("BUY_FAIL_MSG"));
-          console.log(e);
-        });
+      try {
+        this.buyStep = 1;
+        await approveSpending(buyPrice, this.me);
+        this.buyStep = 2;
+        await buyItem(this.itemId, this.me);
+        this.buyStep = 3;
+        alert(this.$t("BUY_SUCCESS_MSG"));
+        setNextPrice(this.itemId, buyPrice);
+      } catch (e) {
+        this.buyStep = -1;
+        alert(this.$t("BUY_FAIL_MSG"));
+        console.log(e);
+      }
     },
     toDisplayedPrice(priceInWei) {
       const { payTokenInfo } = this;
