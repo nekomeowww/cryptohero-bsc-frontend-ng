bb53728e82601cc4e7ab9214cc9a86dc294e0a1e
lychees <lychees67@gmail.com>
2020-10-07 23:16:00 -0700

    Update web3.js

--- src/web3.js
+++ src/web3.js
@@ -1,6 +1,15 @@
 import Web3 from "web3";
 
-const web3 = new Web3(`https://data-seed-prebsc-1-s1.binance.org:8545`);
+
+// const web3 = new Web3(`https://data-seed-prebsc-1-s1.binance.org:8545`);
+
+const web3 = new Web3(
+  Web3.givenProvider ||
+    new Web3.providers.HttpProvider(
+      `https://data-seed-prebsc-1-s1.binance.org:8545`      
+      //`https://rinkeby.infura.io/v3/${process.env.VUE_APP_InfuraID}`
+    )
+);
 
 export const ethEnabled = async () => {
   if (window.ethereum) {
